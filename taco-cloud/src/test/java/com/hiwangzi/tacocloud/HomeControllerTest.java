292ada9aba564e656a846d65b155334c4cae1d80
zill057 <hiwangzi@outlook.com>
2020-03-22 08:24:21 +0800

    add test for home page

---
+++ taco-cloud/src/test/java/com/hiwangzi/tacocloud/HomeControllerTest.java
@@ -0,0 +1,28 @@
+package com.hiwangzi.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHome() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("home"))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        org.hamcrest.Matchers.containsString("Welcome to..."))
+                );
+    }
+}
