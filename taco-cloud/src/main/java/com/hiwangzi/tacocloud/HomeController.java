a3ad552617324ace5b9421e47253f7baee7deb2b
zill057 <hiwangzi@outlook.com>
2020-03-22 08:47:46 +0800

    use @Component replace @Controller

--- taco-cloud/src/main/java/com/hiwangzi/tacocloud/HomeController.java
+++ taco-cloud/src/main/java/com/hiwangzi/tacocloud/HomeController.java
@@ -1,9 +1,11 @@
 package com.hiwangzi.tacocloud;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@RequestMapping("/") // 如果使用 @Controller 注解，可以不需要 @RequestMapping 注解
+@Component
 public class HomeController {
 
     @GetMapping("/")
